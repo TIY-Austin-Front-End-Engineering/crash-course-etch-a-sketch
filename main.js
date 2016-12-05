b10f42feab92df2aed3196b8c823a47f06d458d8
Jess Scheuring <jscheuring@gmail.com>
2016-12-05 16:47:27 -0600

    mvp

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
+var up = document.querySelector('#right .clockwise');
+var down = document.querySelector('#right .counter-clockwise');
+var right = document.querySelector('#left .clockwise');
+var left = document.querySelector('#left .counter-clockwise');
+var x = 6;
+var y = 6;
+
 var canvas = document.getElementById('sketch-canvas');
+canvas.height = canvas.clientHeight;
+canvas.width = canvas.clientWidth;
 var ctx = canvas.getContext('2d');
 
-ctx.fillColor = '#6B808E';
+ctx.strokeStyle = '#6B808E';
+ctx.lineWidth = 1;
+ctx.beginPath();
+ctx.moveTo(x,y)
+
+
+up.addEventListener('click', function() {
+	y--;
+	ctx.lineTo(x, y);
+	ctx.stroke();
+})
+down.addEventListener('click', function() {
+	y++;
+	ctx.lineTo(x,y);
+	ctx.stroke();
+})
+right.addEventListener('click', function() {
+	x++;
+	ctx.lineTo(x,y);
+	ctx.stroke();
+})
+left.addEventListener('click', function() {
+	x--;
+	ctx.lineTo(x,y);
+	ctx.stroke();
+})
